35b62283acdee232aba28549f98529c3feb97191
Кошарный Никита <ntkst73@mail.ru>
2020-11-15 12:26:46 +0400

    Запрос на вытягивание данных с json

--- js/script.js
+++ js/script.js
@@ -18,3 +18,32 @@ console.log(clone);
 
 // калькулятор конвертированиявалюты
 
+const inputRub = document.querySelector('#rub'),
+      inputUsd = document.querySelector('#usd');
+
+      inputRub.addEventListener('input', () => {
+        const request = new XMLHttpRequest(); //запрос
+        //методы
+        request.open('GET', 'js/current.json');  // собираем настрйоки
+        request.setRequestHeader('Content-type', 'application/json; charset=utf-8'); //тип
+        request.send();  // принимает данные
+        
+        //свойства
+        /* 
+        status  200 успешно 
+        statusText 200 успешно
+        response ответ от сервера
+        readyState состояние запроса  1 -был вызван 4 завершено
+        */
+
+        request.addEventListener('load', () => {
+            if (request.status === 200) {
+                const data = JSON.parse(request.response);
+                inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+            }
+
+            else {
+                inputUsd.value = "Что то пошло не так";
+            }
+        });
+    });
